fcc551092bb302294fa763f85c64d000cfb2c2b1
Jimish Ravat <jimishravat2802@gmail.com>
2025-11-24 23:54:13 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NODE_ENV === 'production' ? '/property-nextjs' : '';
+const basePath = "";
 
 const nextConfig = {
   output: 'export',
